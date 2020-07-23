e51f959aad1d23cde70b937534846b9c4df4fade
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-22 23:10:34 -0300

    pagos ok

---
+++ procesar-pago.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+if(isset($_REQUEST["back_url"])){
+	$id = $_REQUEST["payment_id"];
+	$json = file_get_contents("https://api.mercadopago.com/v1/payments/{$id}?access_token=APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
+	$obj = json_decode($json);
+	if( isset($_SESSION["transfer"])){
+		unset($_SESSION["transfer"]);
+	}
+	$_SESSION["transfer"]["transaction_amount"] = $obj->transaction_amount;
+	$_SESSION["transfer"]["payment_id"] = $id;
+
+	header('Location: '.$_REQUEST["back_url"]);
+	die();
+}
+
+echo "<pre>";
+var_dump($_POST);
+echo "</pre>"
+
+?>
