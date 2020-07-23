5f9102dcca4a1605ebab809d121c92d95479a159
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-23 04:19:20 -0300

    Update notifications.php

--- notifications.php
+++ notifications.php
@@ -1,5 +1,10 @@
 <?php
 
+    $archivo = fopen('testigo.log','a');
+	fwrite($archivo,"texto cualquiera");
+	fwrite($archivo,"\n");
+	fclose($archivo);
+
 require __DIR__ .  '/vendor/autoload.php';
    
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
