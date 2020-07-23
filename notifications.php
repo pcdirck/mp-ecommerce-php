dbe9fc3b834fbf6c44097b63da8a42ba615e211c
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-23 03:52:11 -0300

    4am

--- notifications.php
+++ notifications.php
@@ -1,31 +1,43 @@
 <?php
 
-header('Content-Type: application/json');
-echo json_encode(['HTTP/1.1 200 OK'], 200);
-file_put_contents('webhooks.log', $_POST);
-
-/*
 require __DIR__ .  '/vendor/autoload.php';
    
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
   
-switch($_POST["type"]){
-	case "payment":
-		$data = MercadoPago\Payment::find_by_id($_POST["id"]);
-	break;
 
-	case "plan":
-		$data = MercadoPago\Plan.find_by_id($_POST["id"]);
-	break;
+if($_REQUEST["id"]){
+	switch($_REQUEST["topic"]){
+		case "payment":
+			$response = MercadoPago\Payment.find_by_id($_REQUEST["id"]);
+		break;
 
-	case "subscription":
-		$data = MercadoPago\Subscription.find_by_id($_POST["id"]);
-	break;
+		case "plan":
+			$response = MercadoPago\Plan.find_by_id($_REQUEST["id"]);        
+		break;
 
-	case "invoice":
-		$data = MercadoPago\Invoice.find_by_id($_POST["id"]);
-	break;
+		case "subscription":
+			$response = MercadoPago\Subscription.find_by_id($_REQUEST["id"]);        
+		break;
+
+		case "invoice":
+			$response = MercadoPago\Invoice.find_by_id($_REQUEST["id"]);        
+		break;        
+	}
+
+    $archivo = fopen('webhooks.log','a');
+	fwrite($archivo,json_encode($_POST,JSON_PRETTY_PRINT));
+	fwrite($archivo,"\n");
+	fwrite($archivo,json_encode($_REQUEST,JSON_PRETTY_PRINT));
+	fwrite($archivo,"\n");
+	fwrite($archivo,$response);
+	fwrite($archivo,"\n");
+	fclose($archivo);
+
+	http_response_code(200);
+	return;
+}else{
+	http_response_code(400);
+	return;
 }
-*/
 
 ?>
