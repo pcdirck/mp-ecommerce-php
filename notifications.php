8c2f234d99faaec705b53f4e9276e6bd8efafe31
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-23 03:29:28 -0300

    Update notifications.php

--- notifications.php
+++ notifications.php
@@ -1,30 +1,31 @@
 <?php
 
+header('Content-Type: application/json');
+echo json_encode(['HTTP/1.1 200 OK'], 200);
+file_put_contents('webhooks.log',json_encode($_POST));
+
+/*
 require __DIR__ .  '/vendor/autoload.php';
    
-  MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
   
-  switch($_POST["type"]) {
-		case "payment":
-			$data = MercadoPago\Payment::find_by_id($_POST["id"]);
-		break;
+switch($_POST["type"]){
+	case "payment":
+		$data = MercadoPago\Payment::find_by_id($_POST["id"]);
+	break;
 
-		case "plan":
-			$data = MercadoPago\Plan.find_by_id($_POST["id"]);
-		break;
+	case "plan":
+		$data = MercadoPago\Plan.find_by_id($_POST["id"]);
+	break;
 
-		case "subscription":
-			$data = MercadoPago\Subscription.find_by_id($_POST["id"]);
-		break;
+	case "subscription":
+		$data = MercadoPago\Subscription.find_by_id($_POST["id"]);
+	break;
 
-		case "invoice":
-			$data = MercadoPago\Invoice.find_by_id($_POST["id"]);
-		break;
-
-	}
-
-	header('Content-Type: application/json');
-	echo json_encode(['HTTP/1.1 200 OK'], 200);
-	file_put_contents('webhooks.log',json_encode($_POST));
+	case "invoice":
+		$data = MercadoPago\Invoice.find_by_id($_POST["id"]);
+	break;
+}
+*/
 
 ?>
