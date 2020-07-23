9500667db6b794e3eef46c04778cc38e822de1ab
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-23 04:47:58 -0300

    Update notifications.php

--- notifications.php
+++ notifications.php
@@ -5,9 +5,10 @@ error_log("===========  REQUEST  ============== ".print_r($_REQUEST, true));
 $entityBody = file_get_contents('php://input');
 error_log("===========  TODO  ============== ".print_r($entityBody, true));
 
+
+require __DIR__ .'/vendor/autoload.php';
+
 /*
-require __DIR__ .  '/vendor/autoload.php';
-   
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
   
 
