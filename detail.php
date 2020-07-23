e19311a147df83bdd5b820b69b1d6fede45da999
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-23 20:14:05 -0300

    Update detail.php

--- detail.php
+++ detail.php
@@ -179,7 +179,8 @@
 										<h3>$<?php echo number_format($_POST['price'],2,",", ".");?></h3>
 										<h3>Disponibles: <?php echo $_POST['unit'];?></h3>
 									</div>
-									<form action="procesar-pago.php" method="POST">
+									
+									<form action="<?php echo $preference->init_point; ?>" method="POST">
 										<script src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js" data-preference-id="<?php echo $preference->id; ?>" data-button-label="Pagar la compra" data-elements-color="#8e44ad">
 									</script>
 									</form>
