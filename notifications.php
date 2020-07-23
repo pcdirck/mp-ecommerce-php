3801066304b355d68de4a4f3e36e5f420dd34e84
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-23 04:58:25 -0300

    Update notifications.php

--- notifications.php
+++ notifications.php
@@ -5,7 +5,6 @@ require __DIR__ .'/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 
-http_response_code(202);
 
 if(isset($_POST["type"]){
 	http_response_code(201);
