8b8709efbc5ab87dc96cdb805e48a09397fb7ce8
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-24 11:08:37 -0300

    2

--- notifications.php
+++ notifications.php
@@ -1,14 +1,30 @@
 <?php
+// Recibir el cuerpo de la petición.
+$input = @file_get_contents("php://input");
+// Parsear el contenido como JSON.
 
+
+file_put_contents(
+    'registro.log',
+    json_encode($input) . PHP_EOL,
+    FILE_APPEND
+);
+
+// Usar los datos del Webhooks para alguna acción.
+
+// Responder
+http_response_code(200);
+
+/*
 require __DIR__ .  '/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 //MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 
-switch($_GET["type"]){
+switch($_POST["type"]){
 	case "payment":
-		$payment = MercadoPago\Payment.find_by_id($_GET["id"]);
+		$payment = MercadoPago\Payment.find_by_id($_POST["id"]);
 	break;
 	case "plan":
 		$plan = MercadoPago\Plan.find_by_id($_POST["id"]);
@@ -23,5 +39,6 @@ switch($_GET["type"]){
 
 echo "<hr>";
 print_r($payment);
+*/
 
 ?>
