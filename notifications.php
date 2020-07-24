ef0b9264e46d448003055ba78a59287756269e67
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-24 10:05:06 -0300

    Update notifications.php

--- notifications.php
+++ notifications.php
@@ -1,22 +1,18 @@
 <?php
 
-$data = json_encode($_REQUEST);
+require __DIR__ .  '/vendor/autoload.php';
 
-$filename = 'webhooks.log';
-$handle = fopen($filename, "w");
-fwrite($handle, $data);
-fclose($handle);
+MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
+MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
-/*
-$data = $_POST['type'];
+$info = json_decode($this->input->raw_input_stream);
+$payment = new MercadoPago\Payment();
+$or_collection_id = $info->data->id;
+$find_info = MercadoPago\Payment::find_by_id($or_collection_id);
+$or_number = $find_info->external_reference;
 
-if (!empty($data)){
-	$filename = 'webhooks.log';
-	$handle = fopen($filename, "w");
-	fwrite($handle, $data);
-	fclose($handle);
-	echo $file;
-}
-*/
+
+var_dump($find_info);
+
 
 ?>
