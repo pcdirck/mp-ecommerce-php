af7907e866c5407de77f147b5bcc5e3e55fff4f7
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-24 10:19:11 -0300

    Update notifications.php

--- notifications.php
+++ notifications.php
@@ -5,14 +5,6 @@ require __DIR__ .  '/vendor/autoload.php';
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
 MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
-/*
-$info = json_decode($this->input->raw_input_stream);
-$payment = new MercadoPago\Payment();
-$or_collection_id = $info->data->id;
-$find_info = MercadoPago\Payment::find_by_id($or_collection_id);
-$or_number = $find_info->external_reference;
-*/
-
 
 switch($_GET["type"]){
 	case "payment":
@@ -28,7 +20,7 @@ switch($_GET["type"]){
         $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
     break;
 }
-
+echo "<hr>";
 var_dump($payment);
 
 ?>
