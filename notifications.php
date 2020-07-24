e43c405d19e0b6e82c31fa2cbb2702e49d81afc1
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-23 21:00:56 -0300

    webhooks

--- notifications.php
+++ notifications.php
@@ -1,42 +1,13 @@
 <?php
 
-require __DIR__ .'/vendor/autoload.php';
+$data = $_POST['type'];
 
-MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-
-
-if($_REQUEST["id"]){
-	switch($_REQUEST["topic"]){
-		case "payment":
-			$response = MercadoPago\Payment.find_by_id($_REQUEST["id"]);
-		break;
-
-		case "mp-connect":
-		break;
-
-		case "plan":
-			$response = MercadoPago\Plan.find_by_id($_REQUEST["id"]);
-		break;
-
-		case "subscription":
-			$response = MercadoPago\Subscription.find_by_id($_REQUEST["id"]);
-		break;
-
-		case "invoice":
-			$response = MercadoPago\Invoice.find_by_id($_REQUEST["id"]);
-		break;
-	}
-
-	$archivo = fopen('webhooks.log','a');
-	fwrite($archivo,$response);
-	fwrite($archivo,"\n");
-	fclose($archivo);
-
-	http_response_code(200);
-	return;
-}else{
-	http_response_code(400);
-	return;
+if (!empty($data)){
+	$filename = 'webhooks.log';
+	$handle = fopen($filename, "w");
+	fwrite($handle, $data);
+	fclose($handle);
+	echo $file;
 }
 
 ?>
