4e3b55ebc0aa55c3d806aa8c28f7ba5eafe2111b
Dirck <68667381+pcdirck@users.noreply.github.com>
2020-07-24 10:27:51 -0300

    Update notifications.php

--- notifications.php
+++ notifications.php
@@ -3,7 +3,7 @@
 require __DIR__ .  '/vendor/autoload.php';
 
 MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398');
-MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
+//MercadoPago\SDK::setIntegratorId("dev_24c65fb163bf11ea96500242ac130004");
 
 
 switch($_GET["type"]){
@@ -20,7 +20,8 @@ switch($_GET["type"]){
         $plan = MercadoPago\Invoice.find_by_id($_POST["id"]);
     break;
 }
+
 echo "<hr>";
-var_dump($payment);
+print_r($payment);
 
 ?>
